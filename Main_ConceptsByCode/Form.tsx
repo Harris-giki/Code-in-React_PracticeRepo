ca394842ffed6b0d57e373948d9a504e9cf3cb50
harris-giki <harris-giki@gmail.com>
2025-06-08 12:02:44 +0500

    Learning Forms

---
+++ Main_ConceptsByCode/Form.tsx
@@ -0,0 +1,16 @@
+//index.css is used for global styles
+
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="" className="form-label"></label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;

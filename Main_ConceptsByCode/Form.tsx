f762818d6f3426c54dc63d6024d448e53c851ccf
harris-giki <harris-giki@gmail.com>
2025-06-08 12:11:13 +0500

    more aboutforms

--- Main_ConceptsByCode/Form.tsx
+++ Main_ConceptsByCode/Form.tsx
@@ -1,14 +1,27 @@
 //index.css is used for global styles
 
-import React from "react";
-
+import React, { useRef } from "react";
+//by defaullt submission of an HTML form posts it to an served therefore a full page reload occurs
+//to prevent that behaviour see the code below
 const Form = () => {
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault(); //prevents the post to the server
+        console.log("submitted");
+      }}
+    >
       <div className="mb-3">
         <label htmlFor="" className="form-label"></label>
         <input id="name" type="text" className="form-control" />
       </div>
+      <div className="mb-3">
+        <label htmlFor="Age" className="form-label"></label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };

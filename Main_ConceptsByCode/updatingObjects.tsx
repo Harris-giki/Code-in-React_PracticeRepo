c029fbc83f6e28949126ccccd851956e9cfd71c1
harris-giki <harris-giki@gmail.com>
2025-06-08 09:50:35 +0500

    Updating Objects

--- Main_ConceptsByCode/updatingObjects.tsx
+++ Main_ConceptsByCode/updatingObjects.tsx
@@ -10,23 +10,27 @@ function App() {
   });
 
   //now wanting to change one of the properties on some event as done below will now work
+  /*
+  const handleClick = () =>
   {
-    /*const handleClick = () =>
-{
-    drink.price =6;
-    setDrink(drink);
-}*/
-
-      const newDrink = {
-        title: drink.title,
-        price: 6,
-      };
-      setDrink(newDrink);
-    };
+      drink.price =6;
+      setDrink(drink);
   }
+  */
+
+  const handleClick = () => {
+    const newDrink = {
+      title: drink.title,
+      price: 6,
+    };
+    setDrink(newDrink);
+  };
+
   return (
     <div>
       <button onClick={handleClick}>ClickMe</button>
     </div>
   );
 }
+
+export default App;

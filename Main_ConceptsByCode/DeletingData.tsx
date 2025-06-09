8c9f3515c9a5c3ca8c68e128ca56f6b421049a07
harris-giki <harris-giki@gmail.com>
2025-06-09 12:56:36 +0500

    deleting data

---
+++ Main_ConceptsByCode/DeletingData.tsx
@@ -0,0 +1,73 @@
+import axios, { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface user {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<user[]>([]);
+  const [error, serError] = useState("");
+  const [loading, setLoading] = useState(false); // state to show loading
+
+  useEffect(() => {
+    const controller = new AbortController(); // a clean up function to cancel the request by async op
+
+    setLoading(true); // start loading
+
+    axios
+      .get<user[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false); // stop loading on success
+      }) //insert here if needed
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        serError(err.message); // error handled here
+        setLoading(false); // stop loading on error
+        //insert here if needed
+      });
+
+    return () => controller.abort(); // cleanup when component unmounts
+  }, []);
+  const deleteUser = (user: user) => {
+    const originalUsers = [...users]; // backup current user list
+    setUsers(users.filter((u) => u.id !== user.id)); // optimistically remove user from UI
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        serError(err.message); // show error
+        setUsers(originalUsers); // rollback UI to previous state
+      });
+  };
+
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {loading && <div className="spinner-border">Loading...</div>}
+      {/* dynamic rendering of loading message */}
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
